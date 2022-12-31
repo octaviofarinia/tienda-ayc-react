3e53cacac9cf847fe948be34949a8172f87fc906
Octavio Fariña <octaviofarinia@gmail.com>
2022-12-31 18:38:43 -0300

    fix greeting

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <NavBar />
-      <ItemListContainer products={products} />
+      <ItemListContainer greeting={'Bienvenido!'} products={products} />
     </div>
   );
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,8 +3,8 @@ import ProductCard from './ProductCard';
 
 const ItemListContainer = ({ greeting, products }) => {
   return (
-    <section className="body-font bg-gray-900 text-gray-400">
-      <h1>{greeting}</h1>
+    <section className="bg-gray-900 text-gray-400">
+      <h1 className="mx-auto w-max pt-10 text-3xl">{greeting}</h1>
       <div className="container mx-auto px-5 py-24">
         <div className="-m-4 flex flex-wrap">
           {products.map((producto) => (
