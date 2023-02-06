7e5afff885160e8443d0c1f7450dd598aa886299
Octavio Fariña <octaviofarinia@gmail.com>
2023-02-06 20:20:53 -0300

    env variables

--- db/firebase.js
+++ db/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBfLH0RSfsYD7C86uqDv-yyXBw1qb1bbcY',
-  authDomain: 'ecommerce-coder-react-3e966.firebaseapp.com',
-  projectId: 'ecommerce-coder-react-3e966',
-  storageBucket: 'ecommerce-coder-react-3e966.appspot.com',
-  messagingSenderId: '633781604101',
-  appId: '1:633781604101:web:6d970464ad474de452716c'
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
